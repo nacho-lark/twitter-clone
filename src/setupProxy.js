13cd8a1819ff5d2f7c754fe2c5beba3a79ac47cc
nacho-lark <73133195+nacho-lark@users.noreply.github.com>
2020-11-11 06:21:46 -0600

    Update setupProxy.js
    el objeto app ya te. lo proporcionan en como parametro de la funcion, solo debes utilizarlo para lo que necesites, en este caso para recibir un GET en el path /api/login

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,23 +1,5 @@
-import * as express from "express";
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-const app = express();
-
-const BASE_URL = "localhost:8080";
-
-module.exports = () => {
-  // Necesito esto?
-  app.use(
-    "/api/login",
-    createProxyMiddleware({
-      target: BASE_URL,
-      changeOrigin: true,
-    })
-  );
-
+module.exports = app => {
   app.get("/api/login", (req, res) => {
     res.json({ token: "sso_token" });
   });
-
-  app.listen(8080);
 };
